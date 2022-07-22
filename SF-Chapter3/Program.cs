3b74c94e941dd20c555884c3178bec519ff6e9f7
Morgul3000 <shokuave3000@gmail.com>
2022-07-22 14:57:12 +0300

    Add project files.

---
+++ SF-Chapter3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SF_Chapter3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Alex";
+            Console.WriteLine(Name);
+
+            Console.ReadLine();
+        }
+    }
+}
